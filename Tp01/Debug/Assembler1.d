c38963aba2334c65fe9858f667e6e9e326b24da2
Uriel Andrade <usplaysbr@gmail.com>
2021-08-13 21:07:28 -0300

    Tp01_170295

---
+++ Tp01/Debug/Assembler1.d
@@ -0,0 +1,28 @@
+Assembler1.d Assembler1.o: .././Assembler1.S \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ D:\Aplicativos\Microship\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328.h \
+ D:\Aplicativos\Microship\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+D:\Aplicativos\Microship\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328.h:
+
+D:\Aplicativos\Microship\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom328p.h:
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\aplicativos\microship\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
---
+++ Tp01/main.c
@@ -0,0 +1,17 @@
+/*
+ * Tp01.c
+ *
+ * Created: 13/08/2021 20:49:19
+ * Author : uri-7
+ */ 
+
+#include <avr/io.h>
+
+extern void plim();
+
+main(){
+	plim();
+	while(1){
+		
+	}
+};
